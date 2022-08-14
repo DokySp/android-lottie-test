4e6bf6801c51effaf59430fa689fc060260044c0
DokySp <uhug@naver.com>
2022-08-14 22:50:14 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android + Lottie Test
+
+## 1. 
+- [참고](https://hello-bryan.tistory.com/402)
+
+## 2. Jetpack Compose
+- [참고](https://velog.io/@heyday_7/Compose%EC%97%90%EC%84%9C-%EB%A1%9C%ED%8B%B0Lottie%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%B4%EC%84%9C-%EC%95%B1-%EC%98%A8%EB%B3%B4%EB%94%A9-%EC%9E%91%EC%97%85%ED%95%98%EA%B8%B0)
+
+## Make Lottie Animation
+- Use Haiku to create simple lottie animation file.
---
+++ app/src/main/java/space/doky/lottie_test/SplashActivity.kt
@@ -0,0 +1,31 @@
+package space.doky.lottie_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import space.doky.lottie_test.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.lottieView.visibility = View.VISIBLE
+        binding.lottieView.speed = 1.5f
+
+        Thread {
+            Thread.sleep(1000)
+            runOnUiThread {
+                binding.lottieView.playAnimation()
+            }
+        }.start()
+
+    }
+
+
+}
